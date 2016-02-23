ff50bfc5bb3f2c99e1da81ea27d442bcec34edcb
computermouth <computermouth@crunchbangplusplus.org>
2016-02-23 14:04:00 -0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SUITE=jessie
+MIRROR=http://ftp.us.debian.org/debian/
+
+function build_debootstrap {
+  if [[ ! -d rootfs.debootstrap ]]; then
+	  echo "=> Bulding base Debian rootfs..."
+	  #sudo qemu-debootstrap --variant=minbase --no-check-gpg --arch=armhf ${SUITE} rootfs.debootstrap/ ${MIRROR}
+	  sudo qemu-debootstrap --no-check-gpg --arch=armhf ${SUITE} rootfs.debootstrap/ ${MIRROR}
+  else
+	  echo "=> Recycling existing base Debian rootfs..."
+  fi
+
+  for a in $(mount |grep $PWD|awk '{print $3}'); do sudo umount $a; done
+  sudo rm -rf rootfs
+  sudo mkdir rootfs
+  sudo cp -a rootfs.debootstrap/* rootfs
+
+  tar -zcvf rootfs.tar.gz rootfs
+}
+
+build_debootstrap || exit $?
