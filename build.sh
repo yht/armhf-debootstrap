00f50e74b5e4a41b33b942401de4af580e054d2e
computermouth <computermouth@crunchbangplusplus.org>
2016-03-28 21:10:13 -0700

    Added option to build crossbuild env

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ SUITE=jessie
 MIRROR=http://ftp.us.debian.org/debian/
 ARCH=armhf
 
+function setup {
 for i in "$@"; do
   case $i in
   	-i)
@@ -24,17 +25,49 @@ for i in "$@"; do
   	  ;;
   esac
 done
-
-
+}
 
 function build_debootstrap {
-  if [[ ! -d rootfs.debootstrap ]]; then
-	  echo "=> Bulding base Debian rootfs..."
-	  sudo qemu-debootstrap --no-check-gpg --arch=$ARCH ${SUITE} rootfs.debootstrap/ ${MIRROR}
-  else
-	  echo "=> Recycling existing base Debian rootfs..."
-  fi
+  echo "=> Bulding base Debian rootfs..."
+  sudo qemu-debootstrap --no-check-gpg --arch=$ARCH ${SUITE} rootfs.debootstrap/ ${MIRROR}
 
+}
+
+function cross_setup {
+  sudo cp /usr/bin/qemu-arm-static rootfs/usr/bin/
+  sudo cp /etc/resolv.conf rootfs/etc/
+
+  sudo touch rootfs/usr/sbin/policy-rc.d
+  sudo chmod a+w rootfs/usr/sbin/policy-rc.d
+  echo >rootfs/usr/sbin/policy-rc.d <<EOF
+echo "************************************" >&2
+echo "All rc.d operations denied by policy" >&2
+echo "************************************" >&2
+exit 101
+EOF
+  sudo chmod 0755 rootfs/usr/sbin/policy-rc.d
+
+
+  # mount proc, sys and dev
+  sudo mount -t proc     chproc  rootfs/proc
+  sudo mount -t sysfs    chsys   rootfs/sys
+
+  sudo chroot rootfs /bin/bash <<EOF
+set -x
+echo -e "\
+\n\
+deb http://emdebian.org/tools/debian/ jessie main\
+" >> /etc/apt/sources.list
+
+  wget -qO - http://emdebian.org/tools/debian/emdebian-toolchain-archive.key | apt-key add -
+  
+  dpkg --add-architecture armhf
+  apt-get update
+  apt-get install crossbuild-essential-armhf
+
+}
+
+function post_strap {
   for a in $(mount |grep $PWD|awk '{print $3}'); do sudo umount $a; done
   sudo rm -rf rootfs
   sudo mkdir rootfs
@@ -43,5 +76,11 @@ function build_debootstrap {
   sudo tar -zvcf rootfs.tar.gz rootfs
 }
 
+setup || exit $?
 build_debootstrap || exit $?
 
+if [[ $ARCH = i386 ]]; then
+  cross_setup || exit $?
+fi
+
+post_strap || exit $?
