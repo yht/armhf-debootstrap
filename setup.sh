0b240e651bc8d77ae167646617f6be096053e37b
computermouth <computermouth@crunchbangplusplus.org>
2017-01-12 12:06:14 -0800

    weird garbage

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 sudo apt-get update
-sudo apt-get install -y qemu-user-static debootstrap
+sudo apt-get install -y qemu-user-static debootstrap wget tar
+
+wget http://kaplan2539.gitlab.io/baumeister/qemu-arm-static.tar.gz
+tar -xf qemu-arm-static.tar.gz usr/bin/qemu-arm-static
+sudo mv usr/bin/qemu-arm-static /usr/bin/qemu-arm-static
